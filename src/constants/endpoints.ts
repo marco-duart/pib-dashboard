e5df7f8edcc874b8e25a780debff9416a96941bd
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-04-29 16:03:31 -0300

    feat: added toast config

---
+++ src/constants/endpoints.ts
@@ -0,0 +1,4 @@
+export const IBGE_API_URL =
+  "https://servicodados.ibge.gov.br/api/v3/agregados/6784/periodos/all/variaveis?localidades=BR";
+export const CENTRAL_BANK_API_URL =
+  "https://api.bcb.gov.br/dados/serie/bcdata.sgs.3698/dados?formato=json&dataInicial=01/01/1996&dataFinal=31/12/2022";
